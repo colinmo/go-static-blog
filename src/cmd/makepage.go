3156ab33cb8073b95e06d34935bb51303cd007ba
Colin Morris <c.morris@griffith.edu.au>
2025-03-12 14:50:49 +1000

    Different icon

--- src/cmd/makepage.go
+++ src/cmd/makepage.go
@@ -865,7 +865,7 @@ func titleWithIcons(fm FrontMatter) string {
 	icons := map[string]string{
 		"&#x1F496;": fm.LikeOf,
 		"&#x1F516;": fm.BookmarkOf,
-		"&#x1F5EA;": fm.InReplyTo,
+		"&#x1F4AC;": fm.InReplyTo,
 		"&#x1F31F;": fm.FavoriteOf,
 		"&#x3003;":  fm.RepostOf,
 	}
